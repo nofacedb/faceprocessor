d47e81dac744687aa81f8fa701d73cbf0363ad35
masyagin1998 <masyagin1998@yandex.ru>
2019-05-12 16:23:19 +0300

    Some fixes

--- src/faceprocessor.py
+++ src/faceprocessor.py
@@ -11,11 +11,247 @@ from re import match
 
 import face_recognition
 import numpy as np
+import yaml
 from PIL import Image
 from aiohttp import web, ClientSession
 
-U_INT64_SIZE = np.dtype(np.uint64).itemsize
-FLOAT64_SIZE = np.dtype(np.float64).itemsize
+
+class HTTPServer:
+    """HTTPServer class handles request for image processing: finding faces and getting facial features."""
+
+    STATUS_BAD_REQUEST = 400
+    STATUS_INTERNAL_SERVER_ERROR = 500
+
+    API_V1_GET_FBS = '/api/v1/get_fbs'
+    API_V1_GET_FFS = '/api/v1/get_ffs'
+    API_V1_PROC_IMG = '/api/v1/proc_img'
+
+    def __init__(self, cfg: dict):
+        self.cfg = cfg
+        app = web.Application(client_max_size=self.cfg.reqmaxsize)
+        app.add_routes([web.put(HTTPServer.API_V1_GET_FBS, self.api_get_fbs),
+                        web.put(HTTPServer.API_V1_GET_FFS, self.api_get_ffs),
+                        web.put(HTTPServer.API_V1_PROC_IMG, self.api_proc_img)])
+        self.app = app
+
+    def run(self):
+        conn_str = self.cfg.socket
+        is_ip_port = match(r'(\d)+\.(\d)+\.(\d)+\.(\d)+:(\d)+', conn_str) is not None
+        if is_ip_port:
+            conn_data = conn_str.split(':')
+            host = conn_data[0]
+            port = int(conn_data[1])
+            web.run_app(self.app, host=host, port=port)
+        else:
+            path = conn_str
+            web.run_app(self.app, path=path)
+
+    RESP_API_V1_PUT_FBS = '/api/v1/put_fbs'
+
+    async def api_get_fbs(self, req: web.Request) -> web.Response:
+        try:
+            body = await req.json()
+            headers = body['headers']
+            addr = headers['src_addr']
+            req_id = body['id']
+            b64_img_buff = body['img_buff']
+            img = np.array(Image.open(BytesIO(b64decode(b64_img_buff))))
+        except KeyError:
+            return web.json_response({
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': True,
+                'error_info': 'invalid request data'
+            }, status=HTTPServer.STATUS_BAD_REQUEST)
+
+        if self.cfg.immed_resp:
+            asyncio.ensure_future(self.api_get_fbs_create_resp(addr, req_id, img))
+            return web.json_response({'headers': {'src_addr': self.src_addr, 'immed': True}})
+
+        try:
+            fbs = await self.get_fbs_from_img(img)
+        except Exception:
+            return web.json_response({
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': True,
+                'error_info': 'unable to get faceboxes from image'
+            }, status=HTTPServer.STATUS_INTERNAL_SERVER_ERROR)
+
+        return web.json_response({
+            'headers': {'src_addr': self.src_addr, 'immed': False},
+            'id': req_id,
+            'error': False,
+            'boxes': fbs
+        })
+
+    async def api_get_fbs_create_resp(self, addr, req_id, img):
+        try:
+            fbs = await self.get_fbs_from_img(img)
+        except Exception:
+            async with ClientSession(
+                    json_serialize=json.dumps) as session:
+                await session.put(addr + HTTPServer.RESP_API_V1_PUT_FBS, json={
+                    'headers': {'src_addr': self.src_addr, 'immed': False},
+                    'id': req_id,
+                    'error': True,
+                    'error_info': 'unable to get faces from image'
+                })
+            return
+
+        async with ClientSession(
+                json_serialize=json.dumps) as session:
+            await session.put(addr + HTTPServer.RESP_API_V1_PUT_FBS, json={
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': False,
+                'boxes': fbs
+            })
+
+    async def get_fbs_from_img(self, img: np.array) -> list:
+        async with self.lock:
+            if self.gpu:
+                fbs = face_recognition.face_locations(img, self.upsamples, 'cnn')
+            else:
+                fbs = face_recognition.face_locations(img, self.upsamples, 'hog')
+        return fbs
+
+    RESP_API_V1_PUT_FBS = '/api/v1/put_ffs'
+
+    async def api_get_ffs(self, req: web.Request) -> web.Response:
+        try:
+            body = await req.json()
+            headers = body['headers']
+            addr = headers['src_addr']
+            req_id = body['id']
+            b64_img_buff = body['img_buff']
+            img = np.array(Image.open(BytesIO(b64decode(b64_img_buff))))
+            fbs = body['boxes']
+        except KeyError:
+            return web.json_response({
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': True,
+                'error_info': 'invalid request data'
+            }, status=HTTPServer.STATUS_BAD_REQUEST)
+
+        if self.cfg.immed_resp:
+            asyncio.ensure_future(self.api_get_ffs_create_resp(addr, req_id, img, fbs))
+            return web.json_response({'headers': {'src_addr': self.src_addr, 'immed': True}})
+
+        try:
+            ffs = await self.get_ffs_from_img(img, fbs)
+        except Exception:
+            return web.json_response({
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': True,
+                'error_info': 'unable to get facial features from image'
+            }, status=HTTPServer.STATUS_INTERNAL_SERVER_ERROR)
+
+        return web.json_response({
+            'headers': {'src_addr': self.src_addr, 'immed': False},
+            'id': req_id,
+            'error': False,
+            'faces': ffs
+        })
+
+    async def api_get_ffs_create_resp(self, addr, req_id, img, fbs):
+        try:
+            ffs = await self.get_ffs_from_img(img, fbs)
+        except Exception:
+            async with ClientSession(
+                    json_serialize=json.dumps) as session:
+                await session.put(addr + HTTPServer.RESP_API_V1_PUT_FBS, json={
+                    'headers': {'src_addr': self.src_addr, 'immed': False},
+                    'id': req_id,
+                    'error': True,
+                    'error_info': 'unable to get facial features from image'
+                })
+            return
+
+        async with ClientSession(
+                json_serialize=json.dumps) as session:
+            await session.put(addr + HTTPServer.RESP_API_V1_PUT_FBS, json={
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': False,
+                'faces': ffs
+            })
+
+    async def get_ffs_from_img(self, img: np.array, fbs) -> list:
+        async with self.lock:
+            ffs = face_recognition.face_encodings(img, fbs, self.cfg.jitters)
+        return [{'box': fbs[i], 'features': ffs[i].tolist()} for i in
+                range(len(ffs))]
+
+    RESP_API_V1_PUT_FBS = '/api/v1/proc_img'
+
+    async def api_proc_img(self, req: web.Request) -> web.Response:
+        try:
+            body = await req.json()
+            headers = body['headers']
+            addr = headers['src_addr']
+            req_id = body['id']
+            b64_img_buff = body['img_buff']
+            img = np.array(Image.open(BytesIO(b64decode(b64_img_buff))))
+        except KeyError:
+            return web.json_response({
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': True,
+                'error_info': 'invalid request data'
+            }, status=HTTPServer.STATUS_BAD_REQUEST)
+
+        if self.cfg.immed_resp:
+            asyncio.ensure_future(self.api_proc_img_create_resp(addr, req_id, img))
+            return web.json_response({'headers': {'src_addr': self.src_addr, 'immed': True}})
+
+        try:
+            ffs = await self.proc_img(img)
+        except Exception:
+            return web.json_response({
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': True,
+                'error_info': 'unable to process image'
+            }, status=HTTPServer.STATUS_INTERNAL_SERVER_ERROR)
+
+        return web.json_response({
+            'headers': {'src_addr': self.src_addr, 'immed': False},
+            'id': req_id,
+            'error': False,
+            'faces': ffs
+        })
+
+    async def api_proc_img_create_resp(self, addr, req_id, img, fbs):
+        try:
+            ffs = await self.proc_img(img)
+        except Exception:
+            async with ClientSession(
+                    json_serialize=json.dumps) as session:
+                await session.put(addr + HTTPServer.RESP_API_V1_PUT_FBS, json={
+                    'headers': {'src_addr': self.src_addr, 'immed': False},
+                    'id': req_id,
+                    'error': True,
+                    'error_info': 'unable to process image'
+                })
+            return
+
+        async with ClientSession(
+                json_serialize=json.dumps) as session:
+            await session.put(addr + HTTPServer.RESP_API_V1_PUT_FBS, json={
+                'headers': {'src_addr': self.src_addr, 'immed': False},
+                'id': req_id,
+                'error': False,
+                'faces': ffs
+            })
+
+    async def proc_img(self, img: np.array) -> list:
+        fbs = await self.get_fbs_from_img(img)
+        ffs = await self.get_ffs_from_img(self, img, fbs)
+        return ffs
+
 
 DESC_STR = r"""FaceRecognition is a simple script, that finds all faces in image
 and returns their coordinates and features vectors.
@@ -23,140 +259,51 @@ and returns their coordinates and features vectors.
 
 
 def parse_args():
-    """parse_args parses all command line arguments"""
     parser = argparse.ArgumentParser(prog='FaceRecognition',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description=DESC_STR)
     parser.add_argument('-v', '--version', action='version', version='%(prog)s v0.1')
     parser.add_argument('-c', '--config', type=str, default='',
                         help='path to yaml config file')
-    parser.add_argument('-s', '--socket', type=str, default='',
-                        help='IP:PORT or path to UNIX-Socket')
-    parser.add_argument('--immedresp', action='store_true',
-                        help='specifies, if server should return answer immediately')
-    parser.add_argument('--reqmaxsize', type=int, default=1024 ** 2,
-                        help='client request max size in bytes (default: %(default)s))')
-    parser.add_argument('-i', '--input', type=str, default='',
-                        help='path to input image')
-    parser.add_argument('-g', '--gpu', action='store_true',
-                        help='use GPU')
-    parser.add_argument('-u', '--upsamples', type=int, default=1,
-                        help='number of upsamples (bigger ->'
-                             ' smaller faces could be found (default: %(default)s))')
-    parser.add_argument('-j', '--jitters', type=int, default=1,
-                        help='number of re-samples (bigger -> better quality '
-                             'of features vectors, but slower (default: %(default)s))')
-    parser.add_argument('-o', '--output', type=str, default='',
-                        help='path to output image')
 
     args = parser.parse_args()
 
     return args
 
 
-def file_face_recognition(args):
-    """file_face_recognition processes only one input image"""
-    try:
-        img = face_recognition.load_image_file(args.input)
-
-        if args.gpu:
-            face_locations = face_recognition.face_locations(img, args.upsamples, 'cnn')
-        else:
-            face_locations = face_recognition.face_locations(img, args.upsamples, 'hog')
-
-        face_encodings = face_recognition.face_encodings(img, face_locations, args.jitters)
-        face_locations = [np.array(fl).astype(np.uint64) for fl in face_locations]
-
-        with open(args.output, 'wb') as out:
-            for i in range(len(face_locations)):
-                face_locations[i].astype(np.uint64).tofile(out)
-                face_encodings[i].astype(np.float64).tofile(out)
-        return 0
-    except IOError:
-        return -1
+class HTTPServerCFG:
+    def __init__(self, cfg: dict):
+        self.socket = cfg['socket']
+        self.write_timeout_ms = cfg['write_timeout_ms']
+        self.read_timeout_ms = cfg['read_timeout_ms']
+        self.immed_resp = cfg['immed_resp']
+        self.req_max_size = cfg['req_max_size']
+        self.key_path = cfg['key_path']
+        self.crt_path = cfg['crt_path']
 
 
-class AppCtx:
-    """AppCtx class is used for scheduling face recognition tasks"""
-
-    def __init__(self, immed_resp: bool, gpu: bool, upsamples: int, jitters: int):
-        self.immed_resp = immed_resp
-        self.gpu = gpu
-        self.upsamples = upsamples
-        self.jitters = jitters
-        self.lock = asyncio.Lock()
-        self.tasks = []
-
-    async def handle(self, req: web.Request) -> web.Response:
-        """handle handles requests to process image"""
-        body = await req.json()
-        headers = body.get('headers')
-        id = body.get('id')
-        b64_img_buff = body.get('img_buff')
-        img_buff = b64decode(b64_img_buff)
-
-        if self.immed_resp:
-            asyncio.ensure_future(self.create_response(headers, id, img_buff))
-            return web.json_response({'headers': {'src_addr': '', 'immed': True}})
-
-        faces = await self.process_img(img_buff)
-        resp = {
-            'headers': {'src_addr': '', 'immed': False},
-            'id': id,
-            'faces': faces
-        }
-        return web.json_response(resp)
-
-    async def create_response(self, headers, id, img_buff):
-        faces = await self.process_img(img_buff)
-        async with ClientSession(
-                json_serialize=json.dumps) as session:
-            await session.put('http://127.0.0.1:10000' + '/api/v1/put_features', json={
-                'headers': {'src_addr': '', 'immed': False},
-                'id': id,
-                'faces': faces
-            })
-        print('process request for id', id)
-
-    async def process_img(self, img_buff):
-        img = Image.open(BytesIO(img_buff))
-        img = np.array(img)
-        async with self.lock:
-            if self.gpu:
-                face_locations = face_recognition.face_locations(img, self.upsamples, 'cnn')
-            else:
-                face_locations = face_recognition.face_locations(img, self.upsamples, 'hog')
-            face_encodings = face_recognition.face_encodings(img, face_locations, self.jitters)
-        return [{'box': face_locations[i], 'features': face_encodings[i].tolist()} for i in
-                range(len(face_encodings))]
+class FaceRecognitionCFG:
+    def __init__(self, cfg: dict):
+        self.gpu = cfg['gpu']
+        self.upsamples = cfg['upsamples']
+        self.jitters = cfg['jitters']
 
 
-def server_face_recognition(args) -> int:
-    """server_face_recognition starts asynchronous face recognition server"""
-    conn_str = args.socket
-    is_ip_port = match(r'(\d)+\.(\d)+\.(\d)+\.(\d)+:(\d)+', conn_str) is not None
-    app_ctx = AppCtx(args.immedresp, args.gpu, args.upsamples, args.jitters)
-    app = web.Application(client_max_size=args.reqmaxsize)
-    app.add_routes([web.put('/', app_ctx.handle)])
-
-    if is_ip_port:
-        conn_data = conn_str.split(':')
-        host = conn_data[0]
-        port = int(conn_data[1])
-        web.run_app(app, host=host, port=port)
-    else:
-        path = conn_str
-        web.run_app(app, path=path)
-
-    return 0
+class CFG:
+    def __init__(self, fcfg: dict):
+        self.http_server_cfg = HTTPServerCFG(fcfg['http_server'])
+        self.face_recognition_cfg = FaceRecognitionCFG(fcfg['face_recognition'])
 
 
 def main():
     """main processes cargs and processes input image or runs face recognition server"""
     args = parse_args()
-    if args.socket == '':
-        return file_face_recognition(args)
-    return server_face_recognition(args)
+    with open(args.config, 'r') as stream:
+        fcfg = yaml.safe_load(stream)
+        cfg = CFG(fcfg)
+
+    http_server = HTTPServer(cfg)
+    http_server.run()
 
 
 if __name__ == "__main__":
